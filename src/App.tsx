4b06af8fdce9fcba046bd3b93224ae04fe727b71
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-06-17 00:59:03 +0900

    不要なコードの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Create Develop Branch.。。。。サンプル２</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App: React.FC = () => {
+  return <div>App</div>;
+};
