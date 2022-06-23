5254276d66451ec33bf0fe338f74cf1c351e1fe9
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-06-23 13:32:45 +0900

    エラー

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
+import { header } from "./components/molecules/header/header";
+import styles from "./App.module.scss";
 
 export const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div className={styles.root}>
+      Trend Movies
+      <header />
+    </div>
+  );
 };
