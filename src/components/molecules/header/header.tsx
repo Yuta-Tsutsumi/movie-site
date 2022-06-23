3e2f95c0f36e54e9b3fbb51ddb230ec3cff49c81
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-06-23 13:33:56 +0900

    ヘッダーの追加

---
+++ src/components/molecules/header/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./header.module.scss";
+
+export const header: React.FC = () => {
+  return (
+    <div className={styles.root}>
+      <div>header</div>
+    </div>
+  );
+};
