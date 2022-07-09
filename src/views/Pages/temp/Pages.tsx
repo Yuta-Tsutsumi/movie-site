267855b0f847b9aef571efe86b2b1f5c2580d11c
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-07-09 11:35:01 +0900

    ディレクトリ構成の修正中

--- src/views/Pages/temp/Pages.tsx
+++ src/views/Pages/temp/Pages.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Header from "../components/Header/Header";
+import Header from "../../components/Header/Header";
 import styles from "./Pages.module.scss";
 
 // type = {};
