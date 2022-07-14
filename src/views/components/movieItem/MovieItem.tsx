4e8e4bcbe2f4846850bff154526ed6c2fcecd558
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-07-15 01:25:34 +0900

    itemのデータの入ったpropsの作成

--- src/views/components/movieItem/MovieItem.tsx
+++ src/views/components/movieItem/MovieItem.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box } from '@material-ui/core';
 
-const MovieItem = () => {
-  return <div>MovieItem</div>;
+export type Props = {
+  item: { id: number; poster_path: string; title: string; overview: string };
+  onMouse: boolean;
 };
 
+const MovieItem: React.VFC<Props> = {
+  item,
+} => {
+  return (
+    <Box
+      cursor={'pointer'}
+      mb={4}
+      position={'relative'}
+      w={'300px'}
+    ></Box>
+  )
 export default MovieItem;
